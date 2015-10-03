ca54d221f98df323be5e24eba5721b8ee050c0da
CodeBall <m@yanzl.net>
2015-10-03 19:36:50 +0800

    backup_10_03

--- APP/Modules/Index/Action/IndexAction.class.php
+++ APP/Modules/Index/Action/IndexAction.class.php
@@ -9,8 +9,8 @@ Class IndexAction extends Action{
     Public function index(){
         $this->gonggao = M('article')->where(array('cid'=>10))->where(array('del'=>0))->field(array('id','title','time'))->order('time desc')->select();
         $this->dongtai = M('article')->where(array('cid'=>8))->where(array('del'=>0))->field(array('id','title','time'))->order('time desc')->select();
-        $this->college_style = M('article')->where(array('cid'=>13))->where(array('del'=>0))->field(array('id','title','time'))->order('time desc')->select();
-        $this->culture = M('article')->where(array('cid'=>14))->where(array('del'=>0))->field(array('id','title','time'))->order('time desc')->select();
+        $this->college_style = M('article')->where(array('cid'=>14))->where(array('del'=>0))->field(array('id','title','time'))->order('time desc')->select();
+        $this->culture = M('article')->where(array('cid'=>15))->where(array('del'=>0))->field(array('id','title','time'))->order('time desc')->select();
 
         $this->display();
     }
